ae7238d61f5aee8bf81aad8d623688a48152dfec
devSenaX <devSenaXCoder@gmail.com>
2021-06-19 23:58:38 +0530

    firebase setup done

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: false,
-  name: 'dev'
+  name: 'dev',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyDlyeFRbvbzrZiz3JLTQw-vbGFvJrl9wwo',
+      authDomain: 'courseapp-dc0c3.firebaseapp.com',
+      projectId: 'courseapp-dc0c3',
+      storageBucket: 'courseapp-dc0c3.appspot.com',
+      messagingSenderId: '682024397504',
+      appId: '1:682024397504:web:2d57aa0ac92929cef7db25'
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name: 'default',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyDlyeFRbvbzrZiz3JLTQw-vbGFvJrl9wwo',
+      authDomain: 'courseapp-dc0c3.firebaseapp.com',
+      projectId: 'courseapp-dc0c3',
+      storageBucket: 'courseapp-dc0c3.appspot.com',
+      messagingSenderId: '682024397504',
+      appId: '1:682024397504:web:2d57aa0ac92929cef7db25'
+    }
+  }
 };
 
 /*
